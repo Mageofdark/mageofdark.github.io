9f931357b4a8045a55c6dd162a3fce7a1a08fd1f
Ignacio Huanca <ignacarp06@gmail.com>
2026-04-28 03:18:30 -0300

    test

--- javascript/validacionForm.js
+++ javascript/validacionForm.js
@@ -1,21 +1,64 @@
 $(document).ready(function () {
 
-    const form = $('form.needs-validation');
+    // REFERENCIAS DOM
+    const $form = $('form.needs-validation');
+    const $btn = $('#submitBtn');
+    const $text = $btn.find('.btn-text');
+    const $spinner = $btn.find('.spinner-border');
 
     // 1_ VALIDACIÓN AL SUBMIT
-    form.on('submit', function (e) {
+    // $form.on('submit', function (e) {
 
-        if (!this.checkValidity()) {
-            e.preventDefault();
-            e.stopPropagation();
-        }
+    //     const form = this;
+    //     const isValid = form.checkValidity();
 
-        $(this).addClass('was-validated');
-    });
+    //     e.preventDefault();
+    //     e.stopPropagation();
 
-    // =========================
+    //     // activa estilos bootstrap
+    //     $(form).addClass('was-validated');
+
+    //     if (!isValid) return;
+
+    //     // =========================
+    //     // SPINNER ON
+    //     // =========================
+    //     $btn.prop('disabled', true);
+    //     $text.addClass('d-none');
+    //     $spinner.removeClass('d-none');
+
+    //     setTimeout(() => {
+
+    //         // =========================
+    //         // RESET FORM
+    //         // =========================
+    //         form.reset();
+
+    //         // QUITAR ESTADOS DE VALIDACIÓN
+    //         $(form)
+    //             .removeClass('was-validated')
+    //             .find('.form-control')
+    //             .removeClass('is-valid is-invalid');
+
+    //         // RESTAURAR BOTÓN
+    //         $btn.prop('disabled', false);
+    //         $text.removeClass('d-none');
+    //         $spinner.addClass('d-none');
+
+    //     }, 1500);
+    // });
+    // $form.on('submit', function (e) {
+
+    //     if (!this.checkValidity()) {
+    //         e.preventDefault();
+    //         e.stopPropagation();
+    //     }
+
+    //     $(this).addClass('was-validated');
+    // });
+
+
     // 2. VALIDACIÓN EN TIEMPO REAL
-    // =========================
 
     // Nombre completo
     $('#nombreCompleto').on('input', function () {
